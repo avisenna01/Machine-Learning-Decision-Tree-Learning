61e8a2e81f7e25f62fb882cea532456ff4d09331
unknown <Ihsan Imaduddin Azhar>
2020-02-13 15:58:29 +0900

    ignore

--- Bagian B/C45.py
+++ Bagian B/C45.py
@@ -1,16 +1,40 @@
-#from DTL import DecissionTree
-
-# class C45(DecissionTree):
-#     def post_prune(self):
-#         return null
-
-#     def target_most_common_attribute(self):
-#         return null
+from DTL import DecisionTree, Node
 import os
 import pandas
 from sklearn import preprocessing
-import sklearn.datasets as dataset
+from sklearn.datasets import load_iris
+
+class C45(DecisionTree):
+
+    def __init__(self, arr_instans, arr_target):
+        DecisionTree.__init__(self, arr_instans, arr_target)
+        self.instances = arr_instans
+        self.targets = arr_target
+        print(self.targets)
+
+    def post_prune(self):
+        return null
+
+    def target_most_common_attribute(self):
+        return null
+
+
 
 # Load Datasets
-data = pandas.read_csv(r"test.csv")
-print(data)
+
+# iris = load_iris()
+
+# #print(iris)
+
+# here = os.path.dirname(os.path.abspath(__file__))
+# filename = os.path.join(here, 'test.csv')
+# tennisData = pandas.read_csv(filename)
+# print(tennisData)
+# # t =  tennisData.mode(axis = 5)
+# # print(t)
+
+# yes = tennisData.loc[tennisData['play'] == 'Yes']
+# no = tennisData.loc[tennisData['play'] == 'No']
+
+
+# print(tennisData.mode().loc[0][1])
--- Bagian B/Driver2.py
+++ Bagian B/Driver2.py
@@ -1,30 +1,36 @@
 import pandas
+import os
 from DTL import Node
-from ID3 import id3 as id_3
+from C45 import C45 as c_45
 from sklearn import preprocessing
 from sklearn.datasets import load_iris
 
 # Load Datasets
 label_encoder = preprocessing.LabelEncoder()
 iris_datasets = load_iris()
-tennis_datasets = pandas.read_csv("play_tennis.csv")
+here = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(here, 'play_tennis.csv')
+tennis_datasets = pandas.read_csv(filename)
 
 # Node testing
 instances = [[0, 1, 2], [2, 1, 0]]
 targets = [1, 0]
-rules = ["== 0"] 
+rules = ["== 0"] #rules
 root = Node(0, instances, targets)
 
 nodes = [Node(1, [instances[0]], [targets[0]])]
 root.set_rule_children(rules, nodes)
+
+
+
 child = root.next_node(instances[0])
 child.set_rule_children(["== 1"], [Node([instances[1]], [targets[1]])])
 
 some_child = root.next_node(instances[0])
 print(some_child.rule_children)
 
-id = id_3(instances,targets)
-id.fit(id.instances,id.targets)
+id = c_45(instances,targets)
+#id.fit(id.instances,id.targets)
 
 # makan[0] = node1
 # makan[1] = node2
