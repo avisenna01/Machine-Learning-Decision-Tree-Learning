66679d5dbf35475c1c7f3780026ab0d75e4fdefa
unknown <Ihsan Imaduddin Azhar>
2020-02-13 19:51:14 +0900

    belum selesai

--- Bagian B/C45.py
+++ Bagian B/C45.py
@@ -15,26 +15,39 @@ class C45(DecisionTree):
     def post_prune(self):
         return null
 
-    def target_most_common_attribute(self):
-        return null
+    def target_most_common_attribute(training_data):
+        yes = training_data.loc[tennisData['play'] == 'Yes']
+        no = training_data.loc[tennisData['play'] == 'No']
+
+        return (yes.mode().loc[0], no.mode().loc[0])
+
+    def continuous_value(training_data, list_col, arr_target, node_p):
+        sorted = training_data.sort_values(by=['outlook'])
+       
+        
+        candidate_index = []
+        for x in range(len(sorted)):
+            current_target = sorted['play'].iloc[x] #nilai target saat ini
+
+            if not(x==1):
+                if not(sorted['play'].iloc[x-1] == sorted['play'].iloc[x]):
+                    candidate_index.append(x)
+                 
+        print(sorted)
+        print(candidate_index)
+
+        return 1
 
 
 
-# Load Datasets
+#Load Datasets
+iris = load_iris()
+here = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(here, 'test.csv')
+tennisData = pandas.read_csv(filename)
+print(tennisData)
 
-# iris = load_iris()
-
-# #print(iris)
-
-# here = os.path.dirname(os.path.abspath(__file__))
-# filename = os.path.join(here, 'test.csv')
-# tennisData = pandas.read_csv(filename)
-# print(tennisData)
-# # t =  tennisData.mode(axis = 5)
-# # print(t)
-
-# yes = tennisData.loc[tennisData['play'] == 'Yes']
-# no = tennisData.loc[tennisData['play'] == 'No']
+tt = C45.continuous_value(tennisData)
+print(tt)
 
 
-# print(tennisData.mode().loc[0][1])
--- Bagian B/Driver2.py
+++ Bagian B/Driver2.py
@@ -5,6 +5,21 @@ from C45 import C45 as c_45
 from sklearn import preprocessing
 from sklearn.datasets import load_iris
 
+[["Sunny","Hot","High","Weak","No"],
+["Sunny","Hot","High","Strong","No"],
+["Overcast","Hot","High","Weak","Yes"],
+["Rain","Mild","High","Weak","Yes"],
+["Rain","Cool","Normal","Weak","Yes"],
+["Rain","Cool","Normal","Strong","No"],
+["Overcast","Cool","Normal","Strong","Yes"],
+["Sunny","Mild","High","Weak","No"],
+["Sunny","Cool","Normal","Weak","Yes"],
+["Rain","Mild","Normal","Weak","Yes"],
+["Sunny","Mild","Normal","Strong","Yes"],
+["Overcast","Mild","High","Strong","Yes"],
+["Overcast","Hot","Normal","Weak","Yes"],
+["Rain","Mild","High","Strong","No"]]
+
 # Load Datasets
 label_encoder = preprocessing.LabelEncoder()
 iris_datasets = load_iris()
