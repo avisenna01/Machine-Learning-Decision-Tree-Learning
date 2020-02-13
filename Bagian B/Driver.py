3f0f9d15d0e0db3da13845720503d28a4d819e6e
Avisenna Abimanyu <theknightmare07@gmail.com>
2020-02-13 14:13:14 +0700

    test with tennis data

--- Bagian B/Driver.py
+++ Bagian B/Driver.py
@@ -3,11 +3,12 @@ from DTL import Node
 from ID3 import id3 
 from sklearn import preprocessing
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 # Load Datasets
 label_encoder = preprocessing.LabelEncoder()
 iris_datasets = load_iris()
-tennis_datasets = pandas.read_csv("play_tennis.csv")
+tennisData = pandas.read_csv("play_tennis.csv")
 
 # Node testing
 instances = [[0, 1, 2], [2, 1, 0]]
@@ -23,8 +24,23 @@ child.set_rule_children(["== 1"], [Node([instances[1]], [targets[1]])])
 some_child = root.next_node(instances[0])
 # print(some_child.rule_children)
 
+tennisAttr = []
+tennisTarget = tennisData['play'].values
+
+tennisOutlook = tennisData['outlook'].values
+tennisTemp = tennisData['temp'].values
+tennisHumidity = tennisData['humidity'].values 
+tennisWind = tennisData['wind'].values
+
+for i in range(len(tennisData)):
+    tennisAttr.append([tennisOutlook[i], tennisTemp[i], tennisHumidity[i], tennisWind[i]])
+    
+tennisTrain, tennisTest, targetTrain, targetTest = train_test_split(tennisAttr, tennisTarget, test_size=0.1, random_state=42)
+
+
 id = id3(instances,targets)
-id.fit(id.instances,id.targets)
+# id.fit(id.instances,id.targets)
+id.fit(tennisTrain,targetTrain)
 
 # makan[0] = node1
 # makan[1] = node2
